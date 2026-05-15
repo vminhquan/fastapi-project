c692844298ef6e7ebc8da2359e7f0e1db3ac8478
Võ Minh Quân <162551918+vminhquan@users.noreply.github.com>
2026-05-16 04:03:40 +0700

    .

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vominhquan/Documents/GitHub/fastapi-project/.venv"
+set -gx VIRTUAL_ENV "/Users/minhquan/Documents/fastapi-project/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -18,7 +19,10 @@ class Settings(BaseSettings):
     SMTP_PASSWORD: str
     
     # Báo cho Pydantic biết phải tự động tìm và đọc file .env
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=str(Path(__file__).parent.parent.parent / ".env"),
+        env_file_encoding="utf-8"
+    )
 
 # Khởi tạo đối tượng settings để các file khác gọi dùng
 settings = Settings()
